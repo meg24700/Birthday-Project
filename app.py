ee29ce5fbc4a9422965668a6f42f0130e201d0e7
meg24700 <megacbcbcb@gmail.com>
2025-04-26 10:09:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -208,4 +209,5 @@ def random_reaction():
     return random.choice(correct_reactions)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
